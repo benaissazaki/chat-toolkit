daff1819184e4ec13ed099dc0cf87d439e9dc69a
lilxak <zakben45@gmail.com>
2022-09-12 16:04:20 +0100

    Fix lyrics.py linting

--- lyrics.py
+++ lyrics.py
@@ -1,39 +1,54 @@
+''' Searches for song lyrics then types them in the keyboard '''
+
 from time import sleep
 import keyboard
 from bs4 import BeautifulSoup
 import requests
 
-def get_lyrics_link(query):
-    res = list(requests.get(f"https://genius.com/api/search/multi?per_page=5&q={query}").json()['response']['sections'])
-    
-    for r in res:
-        if r['type'] == 'song':
-            for rr in r['hits']:
-                if rr['type'] == 'song':
-                    return rr['result']['url']
+
+def get_lyrics_link(query: str) -> str:
+    ''' Get links to the @query song Genius lyrics page (takes first search result) '''
+
+    results = list(requests.get(
+        f"https://genius.com/api/search/multi?per_page=5&q={query}", timeout=2000)
+        .json()['response']['sections']
+    )
+
+    for result in results:
+        if result['type'] == 'song':
+            for hit in result['hits']:
+                if hit['type'] == 'song':
+                    return hit['result']['url']
+
+    return None
+
 
 def get_lyrics(query):
+    ''' Get lyrics to @query song '''
+
     link = get_lyrics_link(query)
-    page = requests.get(link).text.replace('<i>', '').replace('</i>', '').replace('<b>', '').replace('</b>', '')
+    page = requests.get(link, timeout=2000).text.replace('<i>', '').replace(
+        '</i>', '').replace('<b>', '').replace('</b>', '')
     soup = BeautifulSoup(page, 'html.parser')
-    lyrics = soup.findAll('div', {'data-lyrics-container': 'true'})
-    lyrics = "".join([s.get_text('\n') for s in lyrics])
-    result = ''
+    raw_lyrics = soup.findAll('div', {'data-lyrics-container': 'true'})
+    raw_lyrics = "".join([s.get_text('\n') for s in raw_lyrics])
+
+    cleaned_lyrics = ''
     delete_mode = False
 
-    for c in lyrics:
-        if c in ['[', '(']:
+    for character in raw_lyrics:
+        if character in ['[', '(']:
             delete_mode = True
 
-        if c in [']', ')']:
+        if character in [']', ')']:
             delete_mode = False
 
-        if (c == '\n' and delete_mode):
+        if (character == '\n' and delete_mode):
             continue
 
-        result += c
+        cleaned_lyrics += character
 
-    return result
+    return cleaned_lyrics
 
 
 if __name__ == '__main__':
@@ -42,19 +57,19 @@ if __name__ == '__main__':
         keyboard.press_and_release('backspace')
         print('Magic key pressed')
         recorded = keyboard.record(until='enter')
-        song_name = ''
+        SONG_NAME = ''
         for e in recorded:
             if e.event_type == keyboard.KEY_DOWN:
                 if e.name == 'space':
-                    song_name += ' '
+                    SONG_NAME += ' '
                 elif e.name == 'backspace':
-                    song_name = song_name[:-1]
+                    SONG_NAME = SONG_NAME[:-1]
                 elif e.name not in ['enter', 'esc']:
-                    song_name += e.name
+                    SONG_NAME += e.name
 
-        print(f"-Searching for song: {song_name}")
-        lyrics = get_lyrics(song_name).split('\n')
-        
+        print(f"-Searching for song: {SONG_NAME}")
+        lyrics = get_lyrics(SONG_NAME).split('\n')
+
         for l in lyrics:
             keyboard.write(l)
             keyboard.press_and_release('enter')
