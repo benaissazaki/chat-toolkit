a53c67553b329a56256fd81be1948c8be976e8bd
lilxak <zakben45@gmail.com>
2022-09-12 16:45:50 +0100

    Fix audio.py linting

--- audio.py
+++ audio.py
@@ -1,19 +1,27 @@
+''' Downloads audio from Youtube video then opens the folder containing it'''
+
 import subprocess
 import os
 import keyboard
 import pytube
 import moviepy.editor as mp
 
+
 def mp4_to_mp3(filename, output_path):
+    ''' Convert mp4 video to mp3 audio '''
     video = mp.AudioFileClip(filename)
     video.write_audiofile(output_path)
     video.close()
 
+
 def download_audio(query):
-    s: pytube.YouTube = pytube.Search(query).results[0]
-    s.streams.filter(type='audio')[0].download(output_path=os.path.join('output', 'audio'), filename='tmp.mp4')
-    mp4_to_mp3(os.path.join('output','audio', 'tmp.mp4'), os.path.join('output', 'audio', 'tmp.mp3'))
-    
+    ''' Download first video matching @query then converts it to mp3 '''
+    first_result: pytube.YouTube = pytube.Search(query).results[0]
+    first_result.streams.filter(type='audio')[0].download(
+        output_path=os.path.join('output', 'audio'), filename='tmp.mp4')
+    mp4_to_mp3(os.path.join('output', 'audio', 'tmp.mp4'),
+               os.path.join('output', 'audio', 'tmp.mp3'))
+
 
 if __name__ == '__main__':
     while True:
@@ -21,19 +29,18 @@ if __name__ == '__main__':
         keyboard.press_and_release('backspace')
         print('Magic key pressed')
         recorded = keyboard.record(until='enter')
-        song_name = ''
+        SONG_NAME = ''
         for e in recorded:
             if e.event_type == keyboard.KEY_DOWN:
                 if e.name == 'space':
-                    song_name += ' '
+                    SONG_NAME += ' '
                 elif e.name == 'backspace':
-                    song_name = song_name[:-1]
+                    SONG_NAME = SONG_NAME[:-1]
                 elif e.name not in ['enter', 'esc', 'maj', 'ctrl']:
-                    song_name += e.name
-        
-        print(f"Searching for song: {song_name}")
-        download_audio(song_name)
+                    SONG_NAME += e.name
+
+        print(f"Searching for song: {SONG_NAME}")
+        download_audio(SONG_NAME)
 
         path = os.path.abspath(os.path.join('output', 'audio', 'tmp.mp3'))
         subprocess.Popen(f'explorer /select,"{path}"')
-
