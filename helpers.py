1984759de17a231313edf958c952329500069237
lilxak <zakben45@gmail.com>
2022-09-12 16:38:28 +0100

    Fix helpers.py linting

--- helpers.py
+++ helpers.py
@@ -1,5 +1,9 @@
+''' Helper functions common to multiple modules '''
+
 import subprocess
 
-def copy_file(file):
+def copy_file(file: str):
+    ''' Copy file to clipboard (Windows only) '''
+
     cmd = f"Set-Clipboard -path {file}"
-    subprocess.run(["powershell", "-command", cmd], shell=True)  # windows specific
+    subprocess.run(["powershell", "-command", cmd], shell=True, check=True)  # windows specific
