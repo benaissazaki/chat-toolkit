6b031baf3d1c739a4c7b6067f4b2c9d5b04fc131
lilxak <zakben45@gmail.com>
2022-09-12 17:31:03 +0100

    Change exit conditions in main program

--- __main__.py
+++ __main__.py
@@ -1,19 +1,18 @@
 ''' Launches all main loops in their own threads '''
 
 from threading import Thread
+import keyboard
 from image import listen_image
 from audio import listen_audio
 from lyrics import listen_lyrics
 
 if __name__ == '__main__':
-    image_thread = Thread(target=listen_image)
-    audio_thread = Thread(target=listen_audio)
-    lyrics_thread = Thread(target=listen_lyrics)
+    image_thread = Thread(target=listen_image, daemon=True)
+    audio_thread = Thread(target=listen_audio, daemon=True)
+    lyrics_thread = Thread(target=listen_lyrics, daemon=True)
 
     image_thread.start()
     audio_thread.start()
     lyrics_thread.start()
 
-    image_thread.join()
-    audio_thread.join()
-    lyrics_thread.join()
+    keyboard.wait('alt + *')
