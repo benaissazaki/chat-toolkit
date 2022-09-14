cb4f8c4556ebd21aceaf392a3978a297254cf712
lilxak <zakben45@gmail.com>
2022-09-14 14:24:49 +0100

    Create Settings class

---
+++ settings.py
@@ -0,0 +1,53 @@
+''' Settings configuration '''
+
+import json
+
+
+class Settings():
+    ''' Class that handles the settings '''
+
+    settings_dict = {}
+    default_settings = {
+        'exit_hotkey': 'alt + *',
+        'image': {
+            'api_key': None,
+            'launch_hotkey': 'alt + 4',
+            'submit_hotkey': 'enter'
+        },
+        'lyrics': {
+            'launch_hotkey': 'alt + 5',
+            'submit_hotkey': 'enter'
+        },
+        'audio': {
+            'launch_hotkey': 'alt + 3',
+            'submit_hotkey': 'enter'
+        }
+    }
+
+    @staticmethod
+    def load_settings():
+        ''' Load settings from settings.json file '''
+
+        with open('settings.json', 'r', encoding='utf-8') as settings_file:
+            Settings.settings_dict = json.load(settings_file)
+
+    @staticmethod
+    def get_setting(setting: str):
+        '''
+            Gets a setting from the dict or returns the default value.
+                @setting is in the form "key.key.key" e.g: image.apikey
+        '''
+        setting = setting.split('.')
+        result = Settings.settings_dict
+        try:
+            for key in setting:
+                result = result[key]
+        except KeyError:
+            result = Settings.default_settings
+            try:
+                for key in setting:
+                    result = result[key]
+            except KeyError as exc:
+                raise Exception(f'Setting {setting} does not exist') from exc
+
+        return result
